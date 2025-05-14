97313c2682c226c09369f1c3fb2362c6b1a4db33
lekiam2023 <27883maikel@gmail.com>
2025-05-14 16:44:00 +0200

    Primeros pasos de maquetacion

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='C-principal'>
+
+      <div className='Menu'>
+          <p>Carta</p>
+        <ul className='lista'>
+            <div className='M-Contenido'><a href=''className='link'><li>Cafés</li></a></div>
+            <div className='M-Contenido'><a href=''className='link'><li>Aguas</li></a></div>
+            <div className='M-Contenido'><a href=''className='link'><li>Vinos</li></a></div>
+            <div className='M-Contenido'><a href=''className='link'><li>Cervezas</li></a></div>
+            <div className='M-Contenido'><a href=''className='link'><li>Refrescos</li></a></div>
+            <div className='M-Contenido'><a href=''className='link'><li>Comidas</li></a></div>
+            <div className='M-Contenido'><a href=''className='link'><li>Pica-Pica</li></a></div>
+          </ul>
+       </div>  
+         
+            <div className='C-ImgInfo'>
+                 <div className="Contenido"></div>
+                 <div className="Contenido"></div>
+                 <div className="Contenido"></div>
+                 <div className="Contenido"></div>
+                 <div className="Contenido"></div>
+                 <div className="Contenido"></div>
+                   <div className="Contenido"></div>
+                 <div className="Contenido"></div>
+
+            </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
